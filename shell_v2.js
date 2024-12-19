b741e4ea8368c3b9a1adac9cc482baa3b3984a1e
Dhanoj0810 <142652835+Dhanoj0810@users.noreply.github.com>
2024-12-19 22:15:50 +0530

    Added echo function

--- shell_v2.js
+++ shell_v2.js
@@ -1,8 +1,34 @@
+let currentDirectory = '~';
+let currentPWD = ['~'];
+
+const echo = function (arrgs) {
+  return arrgs.join(' ').replaceAll('"', '');
+}
+
+const promptMessage = function () {
+  return 'dhanojchery@Dhanojs-MacBook-Pro ' + currentDirectory + ' %';
+}
+
+const commands = ['echo', 'which', 'cd'];
+const commandFunctions = [echo, 'which', 'cd'];
+
+const runCommand = function (commandStatement) {
+  const [command, ...args] = commandStatement.split(' ');
+
+  if (commands.includes(command)) {
+    const commandIndex = commands.indexOf(command);
+    return commandFunctions[commandIndex](args);
+  }
+
+  return 'command not found';
+}
+
 const shell = function () {
   while (true) {
-    const command = prompt('dhanojchery@Dhanojs-MacBook-Pro %');
-    
-    console.log('Command :', command);
+    const command = prompt(promptMessage());
+    const resultOfCommand = runCommand(command);
+
+    console.log(resultOfCommand);
   }
 }
 
